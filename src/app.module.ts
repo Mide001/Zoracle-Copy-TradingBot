578710a3031403187c9772f8937959ef0dea2cf0
Mide001 <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>
2025-10-30 02:51:50 +0100

    chore(app): import new modules and bootstrap configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AlchemyWebhookModule } from './modules/alchemy-webhook/alchemy-webhook.module';
+import { MonitoringModule } from './modules/monitoring/monitoring.module';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('mongodb.uri'),
+      }),
+      inject: [ConfigService],
+    }),
+    AlchemyWebhookModule,
+    MonitoringModule,
+  ],
 })
 export class AppModule {}

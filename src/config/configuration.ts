28a48ddbc9ce1e442245022090a129c4d39923b6
Mide001 <olamideisreal5@gmail.com>
2025-10-30 03:10:44 +0100

    config: default PORT to 8080 and use ALCHEMY_AUTH_TOKEN

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,9 +1,9 @@
 export default () => ({
-  port: parseInt(process.env.PORT ?? '3000', 10),
+  port: parseInt(process.env.PORT ?? '8080', 10),
   host: process.env.HOST || '0.0.0.0',
   alchemy: {
     signingKey: process.env.ALCHEMY_SIGNING_KEY,
-    authToken: process.env.ALCHEMY_API_KEY,
+    authToken: process.env.ALCHEMY_AUTH_TOKEN,
     webhookId: process.env.ALCHEMY_WEBHOOK_ID,
     apiBaseUrl: 'https://dashboard.alchemy.com/api',
   },
